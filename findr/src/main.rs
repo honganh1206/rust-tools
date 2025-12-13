be3f2073125569484be3f9e7a6aed997aaf2c146
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-12-13 12:20:04 +0700

    findr: basic arg parsing with tests

---
+++ findr/src/main.rs
@@ -0,0 +1,113 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+use walkdir::{DirEntry, WalkDir};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+// These enums implement the following traits?
+#[derive(Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    // List of regex expressiosn
+    names: Vec<Regex>,
+    entry_types: Vec<EntryType>,
+}
+
+fn main() {
+    if let Err(e) = get_args().and_then(run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn get_args() -> MyResult<Config> {
+    let matches = App::new("findr")
+        .version("0.1.0")
+        .author("Hong Anh Pham")
+        .about("Rust find")
+        .arg(
+            Arg::with_name("paths")
+                .value_name("PATH")
+                .help("Search paths")
+                .default_value(".")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("names")
+                .value_name("NAME")
+                .short("n")
+                .long("name")
+                .help("Name")
+                .takes_value(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("types")
+                .value_name("TYPE")
+                .short("t")
+                .long("type")
+                .help("Entry type")
+                // Ground values for arg
+                .possible_values(&["f", "d", "l"])
+                .takes_value(true)
+                .multiple(true),
+        )
+        .get_matches();
+
+    let names = matches
+        .values_of_lossy("names")
+        .map(|vals| {
+            // From collection to consuming iterator
+            vals.into_iter()
+                // Compile each regex expr
+                .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
+                .collect::<Result<Vec<_>, _>>()
+        })
+        // Change Option to result
+        .transpose()?
+        // Get the Some inside Result?
+        .unwrap_or_default();
+
+    let entry_types = matches
+        .values_of_lossy("types")
+        .map(|vals| {
+            vals.iter()
+                .map(|val| match val.as_str() {
+                    // Pattern matching with enum types
+                    // we must implement cases for all enum values
+                    "d" => Dir,
+                    "f" => File,
+                    "l" => Link,
+                    _ => unreachable!("Invalid type"),
+                })
+                .collect()
+        })
+        .unwrap_or_default();
+
+    Ok(Config {
+        paths: matches.values_of_lossy("paths").unwrap(),
+        names,
+        entry_types,
+    })
+}
+
+fn run(config: Config) -> MyResult<()> {
+    for path in config.paths {
+        for entry in WalkDir::new(path) {
+            match entry {
+                Err(e) => eprintln!("{}", e),
+                Ok(entry) => println!("{}", entry.path().display()),
+            }
+        }
+    }
+    Ok(())
+}
