205badb6bedc680e96922a44ad38d86a44a85882
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-12-19 21:22:47 +0700

    tailr: add parse_num method

--- tailr/src/main.rs
+++ tailr/src/main.rs
@@ -1,3 +1,250 @@
+use crate::TakeValue::*;
+use clap::{App, Arg};
+/// Create lazily evaluated statics (created when 1st use)
+use once_cell::sync::OnceCell;
+use regex::Regex;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+static NUM_RE: OnceCell<Regex> = OnceCell::new();
+
+#[derive(Debug, PartialEq)]
+enum TakeValue {
+    // +0 means everything should be selected
+    PlusZero,
+    // We also need to store negative numbers
+    TakeNum(i64),
+}
+
+#[derive(Debug)]
+struct Config {
+    files: Vec<String>,
+    lines: TakeValue,
+    bytes: Option<TakeValue>,
+    quiet: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = get_args().and_then(run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn get_args() -> MyResult<Config> {
+    let matches = App::new("tailr")
+        .version("0.1.0")
+        .author("Hong Anh Pham")
+        .about("Rust tail")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .conflicts_with("lines")
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .long("quiet")
+                .help("Suppress headers"),
+        )
+        .get_matches();
+
+    let lines = matches
+        .value_of("lines")
+        .map(parse_num)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_num)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+        quiet: matches.is_present("quiet"),
+    })
+}
+
+fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match File::open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => {
+                let (total_lines, total_bytes) = count_lines_bytes(&filename)?;
+                println!(
+                    "{} has {} lines and {} bytes",
+                    filename, total_lines, total_bytes
+                );
+            }
+        }
+    }
+    Ok(())
+}
+
+fn parse_num(val: &str) -> MyResult<TakeValue> {
+    // Optionally capturing preceding group (+ or -)
+    let num_re = NUM_RE.get_or_init(|| Regex::new(r"^([+-])?(\d+)$").unwrap());
+
+    match num_re.captures(val) {
+        Some(caps) => {
+            // Return the matched preceding group (either + or -)
+            let sign = caps.get(1).map_or("-", |m| m.as_str());
+            let num = format!("{}{}", sign, caps.get(2).unwrap().as_str());
+
+            // Ok(val) is a pattern, and we do pattern matching here.
+            // Continue execution if parsing succeeds
+            if let Ok(val) = num.parse() {
+                if sign == "+" && val == 0 {
+                    Ok(PlusZero)
+                } else {
+                    Ok(TakeNum(val))
+                }
+            } else {
+                Err(From::from(val))
+            }
+        }
+        _ => Err(From::from(val)),
+    }
+}
+
+// Read a file from a given byte or line location
+// and return the total number of lines and bytes
+fn count_lines_bytes(filename: &str) -> MyResult<(i64, i64)> {
+    // Check if user requests more lines or bytes than the file contains
+    unimplemented!()
+}
+
+fn print_lines(mut file: impl BufRead, num_lines: &TakeValue, total_lines: i64) -> MyResult<()> {
+    // We can find the starting line's index using num_lines and total_lines?
+    unimplemented!();
+}
+
+// Find the starting byte position?
+fn get_start_index(take_val: &TakeValue, total: i64) -> Option<u64> {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{TakeValue::*, count_lines_bytes, get_start_index, parse_num};
+
+    #[test]
+    fn test_parse_num() {
+        // Default: All integers should be interpreted as negative numbers
+        let res = parse_num("3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        // A leading "+" should result in a positive number
+        let res = parse_num("+3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(3));
+
+        // An explicit "-" value should result in a negative number
+        let res = parse_num("-3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        let res = parse_num("0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(0));
+
+        // Plus zero means select everything
+        let res = parse_num("+0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), PlusZero);
+
+        // Test boundaries
+        let res = parse_num(&i64::MAX.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+        let res = parse_num(&(i64::MIN + 1).to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+        let res = parse_num(&format!("+{}", i64::MAX));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MAX));
+        let res = parse_num(&i64::MIN.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN));
+
+        // A floating-point value is invalid
+        let res = parse_num("3.14");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "3.14");
+        // Any noninteger string is invalid
+        let res = parse_num("foo");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "foo");
+    }
+
+    #[test]
+    fn test_count_lines_bytes() {
+        let res = count_lines_bytes("tests/inputs/one.txt");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), (1, 24));
+        let res = count_lines_bytes("tests/inputs/ten.txt");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), (10, 49));
+    }
+
+    #[test]
+    fn test_get_start_index() {
+        // +0 from an empty file (0 lines/bytes) returns None
+        assert_eq!(get_start_index(&PlusZero, 0), None);
+
+        // +0 from a nonempty file returns an index that
+        // is one less than the number of lines/bytes
+        // because index starts at 0?
+        assert_eq!(get_start_index(&PlusZero, 1), Some(0));
+
+        // Taking 0 lines/bytes returns None
+        assert_eq!(get_start_index(&TakeNum(0), 1), None);
+
+        // Taking any lines/bytes from an empty file returns None
+        assert_eq!(get_start_index(&TakeNum(1), 0), None);
+
+        // Taking more lines/bytes than is available returns None
+        assert_eq!(get_start_index(&TakeNum(2), 1), None);
+
+        // When starting line/byte is less than total lines/bytes,
+        // return one less than starting number
+        assert_eq!(get_start_index(&TakeNum(1), 10), Some(0));
+        assert_eq!(get_start_index(&TakeNum(2), 10), Some(1));
+        assert_eq!(get_start_index(&TakeNum(3), 10), Some(2));
+
+        // When starting line/byte is negative and less than total,
+        // return total - start
+        assert_eq!(get_start_index(&TakeNum(-1), 10), Some(9));
+        assert_eq!(get_start_index(&TakeNum(-2), 10), Some(8));
+        assert_eq!(get_start_index(&TakeNum(-3), 10), Some(7));
+
+        // When starting line/byte is negative and more than total,
+        // return 0 to print the whole file
+        assert_eq!(get_start_index(&TakeNum(-20), 10), Some(0));
+    }
 }
