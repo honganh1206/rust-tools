a59e6845d2b6d45ed4bf1950a442240f5e134873
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-12-20 22:21:20 +0700

    fortuner: add UT and parse u64 value

---
+++ fortuner/src/main.rs
@@ -0,0 +1,101 @@
+use clap::{App, Arg};
+use regex::{Regex, RegexBuilder};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+struct Config {
+    sources: Vec<String>,
+    pattern: Option<Regex>,
+    // Control random selections
+    seed: Option<u64>,
+}
+
+fn main() {
+    if let Err(e) = get_args().and_then(run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn get_args() -> MyResult<Config> {
+    let matches = App::new("fortuner")
+        .version("0.1.0")
+        .author("Hong Anh Pham")
+        .about("Rust fortune")
+        .arg(
+            Arg::with_name("sources")
+                .value_name("FILE")
+                .multiple(true)
+                .required(true)
+                .help("Input files or directories"),
+        )
+        .arg(
+            Arg::with_name("pattern")
+                .value_name("PATTERN")
+                .short("m")
+                .long("pattern")
+                .help("Pattern"),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .short("i")
+                .long("insensitive")
+                .help("Case-insensitive pattern matching")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("seed")
+                .value_name("SEED")
+                .short("s")
+                .long("seed")
+                .help("Random seed"),
+        )
+        .get_matches();
+
+    let pattern = matches
+        .value_of("pattern")
+        .map(|val| {
+            RegexBuilder::new(val)
+                .case_insensitive(matches.is_present("insensitive"))
+                .build()
+                .map_err(|_| format!("Invalid --pattern \"{}\"", val))
+        })
+        .transpose();
+
+    Ok(Config {
+        sources: matches.values_of_lossy("sources").unwrap(),
+        seed: matches.value_of("seed").map(parse_u64).transpose()?,
+        pattern: pattern?,
+    })
+}
+
+fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_u64(val: &str) -> MyResult<u64> {
+    val.parse()
+        .map_err(|_| format!("\"{}\" not a valid integer", val).into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_u64;
+    #[test]
+    fn test_parse_u64() {
+        let res = parse_u64("a");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "\"a\" not a valid integer");
+
+        let res = parse_u64("0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 0);
+
+        let res = parse_u64("4");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 4);
+    }
+}
