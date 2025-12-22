a7b0978e45bd0c4690c90050bab79abd4d597bc3
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-12-22 12:38:31 +0700

    lsr: add methods to format output and validate long flag output

--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg};
 use std::{error::Error, path::PathBuf};
+use tabular::{Row, Table};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -63,9 +64,55 @@ fn find_files(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf>> {
     unimplemented!();
 }
 
+// Validate output with long flag
+fn format_output(paths: &[PathBuf]) -> MyResult<String> {
+    let fmt = "{:<}{:<} {:>} {:<} {:<} {:>} {:<} {:<}";
+    let mut table = Table::new(fmt);
+    for path in paths {
+        table.add_row(
+            Row::new()
+                .with_cell("") // 1 "d" or "-"
+                .with_cell("") // 2 permissions
+                .with_cell("") // 3 number of links
+                .with_cell("") // 4 user name
+                .with_cell("") // 5 group name
+                .with_cell("") // 6 size
+                .with_cell("") // 7 modification
+                .with_cell(""), // 8 path
+        );
+    }
+    Ok(format!("{}", table))
+}
+
+/// Given a file mode in octal format like 0o751,
+/// return a string like "rwxr-x--x"
+fn format_mode(mode: u32) -> String {
+    unimplemented!();
+}
+
+// Validate output for file
+fn long_match(line: &str, expected_name: &str, expected_perms: &str, expected_size: Option<&str>) {
+    let parts: Vec<_> = line.split_whitespace().collect();
+    // why this range?
+    assert!(parts.len() > 0 && parts.len() <= 10);
+
+    let perms = parts.get(0).unwrap();
+    assert_eq!(perms, &expected_perms);
+
+    if let Some(size) = expected_size {
+        // Isn't 4 username?
+        let file_size = parts.get(4).unwrap();
+        assert_eq!(file_size, &size);
+    }
+
+    let display_name = parts.last().unwrap();
+    assert_eq!(display_name, &expected_name);
+}
+
 #[cfg(test)]
 mod test {
-    use super::find_files;
+    use super::{find_files, format_mode, format_output, long_match};
+    use std::path::PathBuf;
 
     #[test]
     fn test_find_files() {
@@ -159,5 +206,50 @@ mod test {
                 "tests/inputs/fox.txt",
             ]
         );
+    }
+
+    #[test]
+    fn test_format_mode() {
+        assert_eq!(format_mode(0o755), "rwxr-xr-x");
+        assert_eq!(format_mode(0o421), "r---w---x");
+    }
+
+    #[test]
+    fn test_format_output_one() {
+        let bustle_path = "tests/inputs/bustle.txt";
+        let bustle = PathBuf::from(bustle_path);
+        let res = format_output(&[bustle]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let lines: Vec<&str> = out.split("\n").filter(|s| !s.is_empty()).collect();
+        assert_eq!(lines.len(), 1);
+
+        let line1 = lines.first().unwrap();
+        long_match(&line1, bustle_path, "-rw-r--r--", Some("193"));
+    }
+
+    #[test]
+    fn test_format_output_two() {
+        let res = format_output(&[
+            PathBuf::from("tests/inputs/dir"),
+            PathBuf::from("tests/inputs/empty.txt"),
+        ]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let mut lines: Vec<&str> = out.split("\n").filter(|s| !s.is_empty()).collect();
+        lines.sort();
+        assert_eq!(lines.len(), 2);
+
+        let empty_line = lines.remove(0);
+        long_match(
+            &empty_line,
+            "tests/inputs/empty.txt",
+            "-rw-r--r--",
+            Some("0"),
+        );
+        let dir_line = lines.remove(0);
+        long_match(&dir_line, "tests/inputs/dir", "drwxr-xr-x", None);
     }
 }
